452db3e4194465e5a2bb3810e69bb3135d5b696f
Toss_swayam <swayam.toss.cs@gmail.com>
2025-08-12 15:27:58 +0530

    Merge remote changes from main into local

--- next.config.mjs
+++ next.config.mjs
@@ -6,13 +6,10 @@ const nextConfig = {
         source: '/api/:path*',
         destination: '/api/:path*',
       },
-<<<<<<< HEAD
       {
         source: '/api/videos/fetch-religious-reels',
         destination: '/api/videos/religious-reels',
       },
-=======
->>>>>>> ba5531e9b34f056c52f9ae9afb3f554ffeef1182
     ];
   },
 };
